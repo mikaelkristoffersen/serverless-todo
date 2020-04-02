8b250408d3d5ee599e439a2448123c414deed481
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-04-03 01:11:05 +0200

    adding missing files

---
+++ packages/www/src/pages/app.js
@@ -0,0 +1,85 @@
+import React, { useContext } from "react";
+import { Router, Link } from "@reach/router";
+import { Container, Flex, Heading, Button, NavLink } from "theme-ui";
+import { IdentityContext } from "../../identity-context";
+
+let DashLoggedOut = props => {
+  const {user, identity: netlifyIdentity } = useContext(IdentityContext);
+
+  return (
+    <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to="/app" p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink href="#!" p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">TODOs</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
+
+let Dash = () => {
+    const {user, identity: netlifyIdentity } = useContext(IdentityContext);
+
+    return (
+      <Container>
+        <Flex as="nav">
+          <NavLink as={Link} to="/" p={2}>
+            Home
+          </NavLink>
+          <NavLink as={Link} to="/app" p={2}>
+            Dashboard
+          </NavLink>
+          {user && (
+          <NavLink
+            href="#!"
+            p={2}
+            onClick={() => {
+              netlifyIdentity.logout();
+            }}
+          >
+            Log out {user.user_metadata.full_name}
+          </NavLink>
+        )}
+        </Flex>
+        <Flex sx={{ flexDirection: "column", padding: 3 }}>
+          <div>User : {user.user_metadata.full_name}</div>
+        </Flex>
+      </Container>
+      );
+};
+
+export default props => {
+  const { user } = useContext(IdentityContext);
+
+  if (!user) {
+    return (
+      <Router>
+        <DashLoggedOut path="/app" />
+      </Router>
+    );
+  }
+  return (
+    <Router>
+      <Dash path="/app" />
+    </Router>
+  );
+};
