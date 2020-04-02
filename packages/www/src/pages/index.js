04ec80e80f45e8345dd4b98f46cec05902abfa8d
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-04-02 17:53:32 +0200

    changed style

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
 
 export default props => (
-    <div>
-        <h1>Our Site</h1>
-    </div>
-)
+  <Container>
+    <Flex sx={{flexDirection: "column", padding: 3}}>
+      <Heading as="h1">TODOs</Heading>
+      <Button
+      sx = {{marginTop: 2}}
+      onClick = {() => alert("Hello!")}
+      > Log In</Button>
+    </Flex>
+  </Container>
+);
