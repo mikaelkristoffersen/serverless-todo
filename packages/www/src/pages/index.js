8b5f7b329573f9431917f329b564768fb031ce8f
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-04-02 22:49:24 +0200

    identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyiIndentity from "netlify-identity-widget";
 
-export default props => (
-  <Container>
-    <Flex sx={{flexDirection: "column", padding: 3}}>
-      <Heading as="h1">TODOs</Heading>
-      <Button
-      sx = {{marginTop: 2}}
-      onClick = {() => alert("Hello!")}
-      > Log In</Button>
-    </Flex>
-  </Container>
-);
+export default props => {
+  useEffect(() => {
+    netlifyiIndentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">TODOs</Heading>
+        <Button sx={{ marginTop: 2 }} onClick={() => netlifyiIndentity.open()}>
+          {" "}
+          Log In
+        </Button>
+        <Button sx={{ marginTop: 2 }} 
+            onClick={() => 
+            console.log(netlifyiIndentity.currentUser())
+            }>
+          {" "}
+          log user
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
